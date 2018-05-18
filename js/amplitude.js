3c77ef9e01c93855ce255f00ff18b8f7f5158228
Katrina Hayes <katrinamargaret.hayes@gmail.com>
2018-05-18 20:53:26 +1000

    updated visual-central circle grows, others stay small

--- js/amplitude.js
+++ js/amplitude.js
@@ -1,22 +1,25 @@
-let circles = [{x: width/2, y: height/2, fill: `rgb(100,${random(100,200)},200)`}];
-
+let circles = [];
+let lastLevel = 0;
+let newMax = 200;
 function draw() {
-
   level = amplitude.getLevel();
-  size = map(level, 0, 1, 0, width/2);
+  size1 = map(level, 0, 1, 0, newMax);
 
   fill('rgb(255,230,230)');
-  ellipse(width/2, height/2, size, size);
+  ellipse(width/2, height/2, size1, size1);
 
+  size2 = map(level, 0,1,0,width/3);
   circles.forEach((circle) => {
     fill(circle.fill);
-    ellipse(circle.x, circle.y, size, size);
+    ellipse(circle.x, circle.y, size2, size2);
   });
 
 
-  if (level > 0.28) {
-    circles.shift();
-    circles.push({x: random(0,width),y: random(0,height),fill: `rgb(100,${random(100,200)},200)`})
+  if (level - lastLevel > 0.2) {
+    circles.push({x: random(0,width),y: random(0,height),fill: `rgb(100,${random(100,200)},200)`});
+    newMax = newMax * 1.1;
   }
-
+  lastLevel = level;
 };
+
+// {x: width/2, y: height/2, fill: `rgb(100,${random(100,200)},200)`}
