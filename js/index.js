b8e804ac8ef54a4a412e10056f690ff08b555698
Katrina Hayes <katrinamargaret.hayes@gmail.com>
2018-05-19 12:22:16 +1000

    switched setup to webaudio api. waveform & bargraph visuals

--- js/index.js
+++ js/index.js
@@ -1,55 +1,44 @@
-// VARIABLES //////
-let song, binCount, bins, smoothing, fft, amplitude;
-let canvas, spectrum, width, height, wSeg, hSeg, level, size;
 
-function preload() {
-  // add load method with the path to your sound
-  song = loadSound('./audio/waltz.mp3');
-};
+$(document).ready(function() {
+  audio.setup();
+  audio.setupSong();
+  waveformAnalysis.setup();
+  bargraphAnalysis.setup();
+  waveform.setup();
+  bargraph.setup();
+  deepsounds.setup();
+  // diagonals.setup();
 
-function setup() {
-  // CHANGE USER CONTROLS TEXT //
-  $('#play-pause').text('Play');
-  $('#stop').text('Stop');
+  // CALL CHOSEN GET DATA FUNCTION ////
+  waveformAnalysis.getByteWaveformData();
+  bargraphAnalysis.getByteFreqData();
+
+  // CALL CORRESONDING UPDATE DATA FUNCTION
+  waveformAnalysis.updateByteWaveform();
+  bargraphAnalysis.updateFreqBarGraph();
+
+  // CALL CORRESPONDING VISUAL
+  waveform.drawByteWaveform();
+  bargraph.drawByteBargraph();
+  deepsounds.draw();
+  // diagonals.drawLines();
+  // diagonals.fill();
+
   // USER CONTROLS FOR PLAYING AUDIO /////////////////////
   $('#play-pause').on('click', function() {
     let text = $('#play-pause').text();
     if (text === 'Play') {
-      song.play();
+      audio.song.play();
 
       $('#play-pause').text('Pause');
     } else {
-      song.pause();
+      audio.song.pause();
       $('#play-pause').text('Play');
     }
   });
   $('#stop').on('click', function() {
-    song.pause();
-    song.currentTime = 0;
+    audio.song.pause();
+    audio.song.currentTime = 0;
     $('#play-pause').text('Play');
   })
-  // CANVAS //
-  canvas = createCanvas(windowWidth, windowHeight); // this sets width & height
-  width = windowWidth;
-  height = windowHeight;
-  canvas.parent('canvas');
-  background(30);
-  // SOUND ANALYSIS: FFT //
-  binCount = 256;
-  bins = new Array(binCount);
-  smoothing = 0.6;
-  fft = new p5.FFT(smoothing, binCount);
-  fft.setInput(song);
-  // SOUND ANALYSIS: AMPLITUDE //
-  amplitude = new p5.Amplitude()
-};
-
-const random = function(min,max) {
-  // getting a random integer:
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
-
-$(document).ready(function() {
-
-
 }); // end of doc ready
